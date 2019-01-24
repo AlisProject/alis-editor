099246eaef2f63b51f7775cd5f33a572c4474c47
y-temp4 <y.temp4@gmail.com>
2019-01-24 11:52:28 +0900

    Add SP editor

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import AlisEditorPc from './AlisEditorPc.vue'
+import AlisEditorSp from './AlisEditorSp.vue'
+
+const renderComponent = window.innerWidth <= 640 ? AlisEditorSp : AlisEditorPc
+
 new Vue({
-  render: (h) => h(AlisEditorPc)
+  render: (h) => h(renderComponent)
 }).$mount('#alis-editor')
