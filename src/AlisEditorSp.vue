abf825a2a2878566493d1e7bcdfe3f8431a326d0
keillera <keikaillera@gmail.com>
2019-03-01 17:13:53 +0900

    Fix typo

--- src/AlisEditorSp.vue
+++ src/AlisEditorSp.vue
@@ -241,8 +241,8 @@ export default {
         () => {
           const targetSelection = window.getSelection()
           if (editor.model.document.selection.hasAttribute('linkHref') && targetSelection) {
-            if (targetSelection.anchorOffset == 0 ||
-              targetSelection.anchorNode.length == targetSelection.anchorOffset) {
+            if (targetSelection.anchorOffset === 0 ||
+              targetSelection.anchorNode.length === targetSelection.anchorOffset) {
               editor.model.change(writer => {
                 writer.removeSelectionAttribute('linkHref')
               })
